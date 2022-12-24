4bb958e440759289dbe99453cca64c86494b561a
clear555 <clarence.sky@yandex.ru>
2022-12-24 21:50:47 +0600

    made a game file

---
+++ game.py
@@ -0,0 +1,17 @@
+from figures.figure import Color
+
+
+class Game:
+    def _init_figures(self):
+        return []
+    def __init__(self):
+        self.figures = []
+        self.is_black_did_castling = False
+        self.is_white_did_castling = False
+        self.move_counter = 0
+        self.move_color = Color.white
+        self.has_check = False
+        self.has_mate = False
+
+    def show(self):
+        pass
